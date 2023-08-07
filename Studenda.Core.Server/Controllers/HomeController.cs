1309aa497452d26f604e933e0e60ac6958a98cb7
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2023-08-08 00:49:23 +0300

    MAKE CONNECTION BETWEEN DB AND SERVER

---
+++ Studenda.Core.Server/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Studenda.Core.Data;
+using Studenda.Core.Data.Configuration;
+using Studenda.Core.Model.Common;
+
+namespace Studenda.Core.Server.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly DataContext context;
+
+        public HomeController(DataContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet("user/{id}")]
+        public IActionResult GetId(int id)
+        {
+            return Json(context.Courses);
+        }
+    }
+}
--- Studenda.Core.Server/Program.cs
+++ Studenda.Core.Server/Program.cs
@@ -1,31 +1,21 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Web;
+using Studenda.Core.Data;
+using Studenda.Core.Data.Configuration;
 
+#if DEBUG
+const bool isDebugMode = true;
+#else
+const bool isDebugMode = false;
+#endif
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ContextConfiguration>(_ => new SqliteConfiguration("Data Source=000_debug_storage.db", isDebugMode));
+builder.Services.AddDbContext<DataContext>(o=>o.UseSqlite());
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
