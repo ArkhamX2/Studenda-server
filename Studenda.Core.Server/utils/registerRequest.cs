6879b03c40d3c792f90372c52885a0435bf0d314
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2023-08-12 02:04:27 +0300

    модели для регистрации

---
+++ Studenda.Core.Server/utils/registerRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Studenda.Core.Server.utils
+{
+    public class registerRequest
+    {
+        public string Email { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Studenda.Core.Server/utils/registerResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Studenda.Core.Server.utils
+{
+    public class registerResponse
+    {
+        
+        
+    }
+}
