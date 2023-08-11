6fc1c57c0ce47db6ac0c2b878006680740007278
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2023-08-12 02:00:41 +0300

    настроил program.cs

--- Studenda.Core.Server/Program.cs
+++ Studenda.Core.Server/Program.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Tokens;
 using Studenda.Core.Data;
 using Studenda.Core.Data.Configuration;
+using Studenda.Core.Server.utils;
+
 
 #if DEBUG
 const bool isDebugMode = true;
@@ -15,7 +18,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ContextConfiguration>(_ => new SqliteConfiguration("Data Source=000_debug_storage.db", isDebugMode));
 builder.Services.AddDbContext<DataContext>(o=>o.UseSqlite());
+builder.Services.AddCors(c => c.AddPolicy("cors", opt =>
+{
+    opt.AllowAnyHeader();
+    opt.AllowCredentials();
+    opt.AllowAnyMethod();
+    opt.WithOrigins(builder.Configuration.GetSection("Cors:Urls").Get<string[]>()!);
+}));
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // указывает, будет ли валидироваться издатель при валидации токена
+            ValidateIssuer = true,
+            // строка, представляющая издателя
+            ValidIssuer = AuthOptions.ISSUER,
+            // будет ли валидироваться потребитель токена
+            ValidateAudience = true,
+            // установка потребителя токена
+            ValidAudience = AuthOptions.AUDIENCE,
+            // будет ли валидироваться время существования
+            ValidateLifetime = true,
+            // установка ключа безопасности
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            // валидация ключа безопасности
+            ValidateIssuerSigningKey = true,
+            ClockSkew=TimeSpan.FromMinutes(2),
+        };
+    });
+
 
 var app = builder.Build();
+app.UseCors("cors");
 app.MapControllers();
 app.Run();
