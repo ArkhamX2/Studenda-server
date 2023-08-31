483e6d5de49cb9d5bd41691e3a2400d4a9aed0c9
Alexander <106086600+dubstepTractor@users.noreply.github.com>
2023-09-01 00:05:25 +0300

    remove unnecessary files

--- Studenda.Core.Server/utils/Token/TokenModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Studenda.Core.Server.Utils.Token
-{
-    public class TokenModel
-    {
-        public string? AccessToken { get; set; }
-        public string? RefreshToken { get; set; }
-    }
-}
