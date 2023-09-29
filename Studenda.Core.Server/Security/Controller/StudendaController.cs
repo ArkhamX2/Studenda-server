35c38ca86c512a53ce35f25ceed9825f8c438bc4
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2023-09-30 02:21:55 +0300

    Create StudendaController.cs

---
+++ Studenda.Core.Server/Security/Controller/StudendaController.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Studenda.Core.Data;
+using Studenda.Core.Model.Account;
+using Studenda.Core.Model.Common;
+using Studenda.Core.Server.Security.Data.Transfer;
+
+namespace Studenda.Core.Server.Security.Controller
+{   
+    [Route("studenda/api")]
+    [ApiController]
+    public class StudendaController
+    {
+        public DataContext DataContext { get; }
+        public IConfiguration Connfiguration { get; }
+
+        public StudendaController(DataContext dataContext, IConfiguration configuration)
+        { 
+            DataContext=dataContext;
+            Connfiguration = configuration;
+        }
+        
+
+        [Route("GetUserFromSecurityResponse")]
+        [HttpGet]
+        public ActionResult<User> GetUserFromSecurityResponse(SecurityResponse response)
+        {
+            var user = DataContext.Users.FirstOrDefault(x => x.Email == response.Email);
+            return user;
+        }
+
+
+    }
+}
