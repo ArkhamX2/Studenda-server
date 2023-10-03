3deefe4b3e6eb82fc14d7707b0620cc891ae34b3
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2023-10-03 23:00:39 +0300

    change routes name and add new routes

--- Studenda.Core.Server/Security/Controller/StudendaController.cs
+++ Studenda.Core.Server/Security/Controller/StudendaController.cs
@@ -5,7 +5,7 @@ using Studenda.Core.Data;
 using Studenda.Core.Model.Common;
 
 namespace Studenda.Core.Server.Security.Controller
-{   
+{
     [Route("studenda/api")]
     [ApiController]
     public class StudendaController
@@ -14,32 +14,51 @@ namespace Studenda.Core.Server.Security.Controller
         public IConfiguration Connfiguration { get; }
 
         public StudendaController(DataContext dataContext, IConfiguration configuration)
-        { 
-            DataContext=dataContext;
+        {
+            DataContext = dataContext;
             Connfiguration = configuration;
         }
-        [Route("Departments")]
+        [Route("departments")]
         [HttpGet]
         public ActionResult<List<Department>> GetAllDepartments()
         {
             var departments = DataContext.Departments.ToList();
             return departments;
         }
-        [Route("Courses")]
+        [Route("courses")]
         [HttpGet]
         public ActionResult<List<Course>> GetAllCourses()
         {
             var Courses = DataContext.Courses.ToList();
             return Courses;
         }
-        [Route("Groups")]
+        [Route("groups")]
         [HttpGet]
         public ActionResult<List<Group>> GetAllGroups()
         {
             var Groups = DataContext.Groups.ToList();
             return Groups;
         }
-
-
+        [Route("department/{id}")]
+        [HttpGet]
+        public ActionResult<Department> GetDepartmentById(int id)
+        {
+            var department = DataContext.Departments.FirstOrDefault(x => x.Id == id);
+            return department;
+        }
+        [Route("course/{id}")]
+        [HttpGet]
+        public ActionResult<Course> GetCourseById(int id)
+        {
+            var Course = DataContext.Courses.FirstOrDefault(x=>x.Id==id);
+            return Course;
+        }
+        [Route("group/{id}")]
+        [HttpGet]
+        public ActionResult<Group> GetGroupById(int id)
+        {
+            var Group = DataContext.Groups.FirstOrDefault(x=>x.Id==id);
+            return Group;
+        }
     }
 }
