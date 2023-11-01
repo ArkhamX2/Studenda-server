09f55eebbdb7b7bf95f5a5a04bf1e75576f97509
Timofey Latypov <timtompson3241@gmail.com>
2023-11-02 00:02:26 +0300

    Schedule and weektype services

---
+++ Studenda.Core.Client/Services/IScheduleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Studenda.Core.Client.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studenda.Core.Client.Services
+{
+    public interface IScheduleRepository
+    {
+        Task<List<Subject>> GetSubjects(int groupId, int weekTypeId);
+    }
+}
