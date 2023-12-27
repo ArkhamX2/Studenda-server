e674692a5c10f555c0c594cb1ca0dee3990d2ca8
Alexander <106086600+dubstepTractor@users.noreply.github.com>
2023-12-27 18:30:53 +0300

    update docker release config and add dotnet 8 target

--- Studenda.Core.Server/Program.cs
+++ Studenda.Core.Server/Program.cs
@@ -19,24 +19,30 @@ const bool isDebugMode = true;
 const bool isDebugMode = false;
 #endif
 
-// TODO: Использовать отдельный класс для работы с конфигурацией.
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", false, true)
-    .Build();
+var applicationBuilder = WebApplication.CreateBuilder(args);
+var configuration = applicationBuilder.Configuration;
 
-var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
-var identityConnectionString = configuration.GetConnectionString("IdentityConnection");
+// Базы данных.
 
-if (string.IsNullOrEmpty(defaultConnectionString) || string.IsNullOrEmpty(identityConnectionString))
+var defaultConnectionString = configuration.GetConnectionString("Default");
+var identityConnectionString = configuration.GetConnectionString("Identity");
+
+if (string.IsNullOrEmpty(defaultConnectionString))
 {
-    throw new Exception("Connection string is null or empty!");
+    throw new Exception("Default connection string is null or empty!");
+}
+
+if (string.IsNullOrEmpty(identityConnectionString))
+{
+    throw new Exception("Identity connection string is null or empty!");
 }
 
 // TODO: Конфигурация контекстов на основе конфигурации приложения.
 var dataConfiguration = new MysqlConfiguration(defaultConnectionString, ServerVersion.AutoDetect(defaultConnectionString), isDebugMode);
-var identityConfiguration = new SqliteConfiguration(identityConnectionString, isDebugMode);
+var identityConfiguration = new MysqlConfiguration(defaultConnectionString, ServerVersion.AutoDetect(defaultConnectionString), isDebugMode);
 
-var applicationBuilder = WebApplication.CreateBuilder(args);
+// Сервисы.
+
 var serviceCollection = applicationBuilder.Services;
 
 serviceCollection.AddSingleton<IContextFactory<DataContext>>(new DataContextFactory(dataConfiguration));
@@ -88,6 +94,8 @@ serviceCollection.Configure<IdentityOptions>(options =>
     options.Password.RequireLowercase = false;
     options.Password.RequireUppercase = false;
 });
+
+// Приложение.
 
 var application = applicationBuilder.Build();
 
