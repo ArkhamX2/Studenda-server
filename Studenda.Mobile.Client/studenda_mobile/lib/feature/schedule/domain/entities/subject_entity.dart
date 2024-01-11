c66972b48a500e97218f7f08120910c1803e18f0
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-01-12 00:01:13 +0300

    mobile remove

--- Studenda.Mobile.Client/studenda_mobile/lib/feature/schedule/domain/entities/subject_entity.dart
+++
@@ -1,7 +0,0 @@
-class SubjectEntity {
-  final int dayPosition;
-  final String name;
-  final String place;
-  final String teacher;
-  SubjectEntity(this.dayPosition,this.name, this.place, this.teacher);
-}
