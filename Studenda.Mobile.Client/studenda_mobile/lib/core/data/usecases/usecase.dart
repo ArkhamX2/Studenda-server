45de20ce66e12be0870ef8a4d85c47f1dff8c4cc
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-01-11 23:34:11 +0300

    try to ignore

--- Studenda.Mobile.Client/studenda_mobile/lib/core/data/usecases/usecase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:studenda_mobile/core/data/error/failure.dart';
-
-abstract class Usecase<Type,Params>{
-  Future<Either<Failure,Type>> call(Params params);
-}
--- Studenda.Mobile.Client/studenda_mobile/lib/model/schedule/Management/week_schedule.dart
+++
@@ -1,6 +0,0 @@
-import 'package:studenda_mobile/model/schedule/Management/day_schedule.dart';
-
-class WeekSchedule {
-  final List<DaySchedule> daySchedules;
-  WeekSchedule(this.daySchedules);
-}
