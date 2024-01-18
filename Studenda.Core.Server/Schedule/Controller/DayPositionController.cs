169cb401f8dcaa35efb00bf64394f0b58bc4dcab
Исламов Мурад Гюльмуса оглы <99683993+S1stemus@users.noreply.github.com>
2024-01-19 00:33:38 +0300

    create Day and  Subject positions controller

---
+++ Studenda.Core.Server/Schedule/Controller/DayPositionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Studenda.Core.Model.Common;
+using Studenda.Core.Server.Common.Service;
+
+namespace Studenda.Core.Server.Schedule.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DayPositionController : ControllerBase
+    {
+        public DayPositionController(DataEntityService dataEntityService)
+        {
+                DataEntityService=dataEntityService;
+        }
+
+        public DataEntityService DataEntityService { get; }
+
+        [HttpGet]
+        public ActionResult<List<Course>> Get([FromBody] List<int> ids)
+        {
+            return DataEntityService.Get(DataEntityService.DataContext.Courses, ids);
+        }
+    }
+}
