28df4e530beea11c5bbcdc4f127ad1b6e2e6ed32
Alexander <106086600+dubstepTractor@users.noreply.github.com>
2024-01-20 13:20:18 +0300

    review and discipline controller

---
+++ Studenda.Core.Server/Schedule/Controller/DisciplineController.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+using Studenda.Core.Model.Schedule.Management;
+using Studenda.Core.Server.Common.Service;
+
+namespace Studenda.Core.Server.Schedule.Controller;
+
+/// <summary>
+///     Контроллер для работы с объектами типа <see cref="Discipline" />.
+/// </summary>
+[Route("api/discipline")]
+[ApiController]
+public class DisciplineController : ControllerBase
+{
+    /// <summary>
+    ///     Конструктор.
+    /// </summary>
+    /// <param name="dataEntityService">Сервис моделей.</param>
+    public DisciplineController(DataEntityService dataEntityService)
+    {
+        DataEntityService = dataEntityService;
+    }
+
+    /// <summary>
+    ///     Сервис моделей.
+    /// </summary>
+    private DataEntityService DataEntityService { get; }
+
+    /// <summary>
+    ///     Получить список дисциплин.
+    ///     Если идентификаторы не указаны, возвращается список со всеми дисциплинами.
+    ///     Иначе возвращается список с указанными дисциплинами, либо пустой список.
+    /// </summary>
+    /// <param name="ids">Список идентификаторов.</param>
+    /// <returns>Результат операции со списком дисциплин.</returns>
+    [HttpGet]
+    public ActionResult<List<Discipline>> Get([FromQuery] List<int> ids)
+    {
+        return DataEntityService.Get(DataEntityService.DataContext.Disciplines, ids);
+    }
+
+    /// <summary>
+    ///     Сохранить дисциплины.
+    /// </summary>
+    /// <param name="entities">Список дисциплин.</param>
+    /// <returns>Результат операции.</returns>
+    [HttpPost]
+    public IActionResult Post([FromBody] List<Discipline> entities)
+    {
+        var status = DataEntityService.Set(DataEntityService.DataContext.Disciplines, entities);
+
+        if (!status)
+        {
+            return BadRequest("No disciplines were saved!");
+        }
+
+        return Ok();
+    }
+
+    /// <summary>
+    ///     Удалить дисциплины.
+    /// </summary>
+    /// <param name="ids">Список идентификаторов.</param>
+    /// <returns>Результат операции.</returns>
+    [HttpDelete]
+    public IActionResult Delete([FromBody] List<int> ids)
+    {
+        var status = DataEntityService.Remove(DataEntityService.DataContext.Disciplines, ids);
+
+        if (!status)
+        {
+            return BadRequest("No disciplines were deleted!");
+        }
+
+        return Ok();
+    }
+}
--- Studenda.Core.Server/Schedule/Controller/SubjectTypeController.cs
+++ Studenda.Core.Server/Schedule/Controller/SubjectTypeController.cs
@@ -1,50 +1,76 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Studenda.Core.Model.Schedule.Management;
 using Studenda.Core.Server.Common.Service;
 
-namespace Studenda.Core.Server.Schedule.Controller
+namespace Studenda.Core.Server.Schedule.Controller;
+
+/// <summary>
+///     Контроллер для работы с объектами типа <see cref="SubjectType" />.
+/// </summary>
+[Route("api/subject-type")]
+[ApiController]
+public class SubjectTypeController : ControllerBase
 {
-    [Route("api/subjecttype")]
-    [ApiController]
-    public class SubjectTypeController : ControllerBase
+    /// <summary>
+    ///     Конструктор.
+    /// </summary>
+    /// <param name="dataEntityService">Сервис моделей.</param>
+    public SubjectTypeController(DataEntityService dataEntityService)
     {
-        public SubjectTypeController(DataEntityService dataEntityService)
+        DataEntityService = dataEntityService;
+    }
+
+    /// <summary>
+    ///     Сервис моделей.
+    /// </summary>
+    private DataEntityService DataEntityService { get; }
+
+    /// <summary>
+    ///     Получить список типов занятия.
+    ///     Если идентификаторы не указаны, возвращается список со всеми типами.
+    ///     Иначе возвращается список с указанными типами, либо пустой список.
+    /// </summary>
+    /// <param name="ids">Список идентификаторов.</param>
+    /// <returns>Результат операции со списком типов.</returns>
+    [HttpGet]
+    public ActionResult<List<SubjectType>> Get([FromQuery] List<int> ids)
+    {
+        return DataEntityService.Get(DataEntityService.DataContext.SubjectTypes, ids);
+    }
+
+    /// <summary>
+    ///     Сохранить типы занятия.
+    /// </summary>
+    /// <param name="entities">Список типов.</param>
+    /// <returns>Результат операции.</returns>
+    [HttpPost]
+    public IActionResult Post([FromBody] List<SubjectType> entities)
+    {
+        var status = DataEntityService.Set(DataEntityService.DataContext.SubjectTypes, entities);
+
+        if (!status)
         {
-            DataEntityService= dataEntityService;
+            return BadRequest("No subject types were saved!");
         }
 
-        public DataEntityService DataEntityService { get; }
+        return Ok();
+    }
 
-        [HttpGet]
-        public ActionResult<List<SubjectType>> Get([FromBody] List<int> ids)
+    /// <summary>
+    ///     Удалить типы занятия.
+    /// </summary>
+    /// <param name="ids">Список идентификаторов.</param>
+    /// <returns>Результат операции.</returns>
+    [HttpDelete]
+    public IActionResult Delete([FromBody] List<int> ids)
+    {
+        var status = DataEntityService.Remove(DataEntityService.DataContext.SubjectTypes, ids);
+
+        if (!status)
         {
-            return DataEntityService.Get(DataEntityService.DataContext.SubjectTypes, ids);
-        }
-        [HttpPost]
-        public IActionResult Post([FromBody] List<SubjectType> entities)
-        {
-            var status = DataEntityService.Set(DataEntityService.DataContext.SubjectTypes, entities);
-
-            if (!status)
-            {
-                return BadRequest("No week types were saved!");
-            }
-
-            return Ok();
-        }
-        [HttpDelete]
-        public IActionResult Delete([FromBody] List<int> ids)
-        {
-            var status = DataEntityService.Remove(DataEntityService.DataContext.SubjectTypes, ids);
-
-            if (!status)
-            {
-                return BadRequest("No week types were deleted!");
-            }
-
-            return Ok();
+            return BadRequest("No subject types were deleted!");
         }
 
+        return Ok();
     }
 }
